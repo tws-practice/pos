cf8ebb36888d1f4c7a6b550736dc2c346ca601e9
Hao Hu <hhu@thoughtworks.com>
2014-08-04 01:50:27 +0800

    add pos_v2

---
+++ pos_v2/spec/fixtures.js
@@ -0,0 +1,20 @@
+function loadAllItems() {
+    return [
+        new Item('ITEM000000', '可口可乐', '瓶', 3.00),
+        new Item('ITEM000001', '雪碧', '瓶', 3.00),
+        new Item('ITEM000002', '苹果', '斤', 5.50),
+        new Item('ITEM000003', '荔枝', '斤', 15.00),
+        new Item('ITEM000004', '电池', '个', 2.00),
+        new Item('ITEM000005', '方便面', '袋', 4.50)
+    ];
+}
+
+function loadPromotions() {
+    return [
+        new Promotion('BUY_TWO_GET_ONE_FREE', [
+            'ITEM000000',
+            'ITEM000001',
+            'ITEM000005'
+        ])
+    ]
+}
---
+++ pos_v2/src/model/item.js
@@ -0,0 +1,6 @@
+function Item(barcode, name, unit, price) {
+    this.barcode = barcode;
+    this.name = name;
+    this.unit = unit;
+    this.price = price || 0.00;
+}
