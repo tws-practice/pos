85662e48f8bffac8692143f29465e49ea71e5f26
ABBY GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUOAB@OOCL.COM>
2018-07-06 09:19:52 +0800

    [GUOAB] modify mainTest.js

--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -15,20 +15,20 @@ describe('pos', () => {
       'ITEM000005-2',
     ];
 
-   spyOn(console, 'log');
+    spyOn(console, 'log');
 
     printReceipt(tags);
 
     const expectText = `***<没钱赚商店>收据***
-名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
-名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
-----------------------
-总计：58.50(元)
-节省：7.50(元)
-**********************`;
+                        名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+                        名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+                        名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+                        ----------------------
+                        总计：58.50(元)
+                        节省：7.50(元)
+                        **********************`;
 
-   expect(console.log).toHaveBeenCalledWith(expectText);
+    expect(console.log).toHaveBeenCalledWith(expectText);
   });
 });
 
@@ -48,13 +48,16 @@ describe('BuildCodeAndNumArray check', () => {
       'ITEM000005-2',
     ];
 
-    const codeAndNumArrayCheck =[{code:'ITEM000001',num:5},{code:'ITEM000003',num:2.5},{code:'ITEM000005',num:3}];
+    const codeAndNumArrayCheck = [{code: 'ITEM000001', num: 5}, {code: 'ITEM000003', num: 2.5}, {
+      code: 'ITEM000005',
+      num: 3
+    }];
 
     spyOn(console, 'log');
 
     let codeAndNumArray = BuildCodeAndNumArray(tags);
     let codeAndNumArrayToJson = JSON.stringify(codeAndNumArray);
-    expect(JSON.stringify(codeAndNumArrayCheck)).toBe(codeAndNumArrayToJson);
+    expect(codeAndNumArrayToJson).toBe(JSON.stringify(codeAndNumArrayCheck));
   });
 });
 
@@ -101,14 +104,19 @@ describe('getReceiptArray check', () => {
         price: 4.50
       }
     ];
-    const recieptArrayCheck =[{barcode:'ITEM000001',name:'雪碧',unit:'瓶',price:3,num:5},{barcode:'ITEM000003',name:'荔枝',
-      unit:'斤',price:15,num:2.5},{barcode:'ITEM000005',name:'方便面',unit:'袋',price:4.5,num:3}];
-    const codeAndNumArray =[{code:'ITEM000001',num:5},{code:'ITEM000003',num:2.5},{code:'ITEM000005',num:3}];
+    const recieptArrayCheck = [
+      {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3, num: 5},
+      {barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15, num: 2.5},
+      {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.5, num: 3}];
+    const codeAndNumArray = [
+      {code: 'ITEM000001', num: 5},
+      {code: 'ITEM000003', num: 2.5},
+      {code: 'ITEM000005', num: 3}];
     spyOn(console, 'log');
 
     let recieptArray = getReceiptArray(allGoodItemArray, codeAndNumArray);
     let recieptArrayToJson = JSON.stringify(recieptArray);
-    expect(JSON.stringify(recieptArrayCheck)).toBe(recieptArrayToJson);
+    expect(recieptArrayToJson).toBe(JSON.stringify(recieptArrayCheck));
   });
 });
 
@@ -119,8 +127,10 @@ describe('getReceiptPreSum check', () => {
 
     const noDiscountTotalPriceCheck = 66;
 
-    const recieptArray =[{barcode:'ITEM000001',name:'雪碧',unit:'瓶',price:3,num:5},{barcode:'ITEM000003',name:'荔枝',
-      unit:'斤',price:15,num:2.5},{barcode:'ITEM000005',name:'方便面',unit:'袋',price:4.5,num:3}];
+    const recieptArray = [
+      {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3, num: 5},
+      {barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15, num: 2.5},
+      {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.5, num: 3}];
 
     spyOn(console, 'log');
 
@@ -135,21 +145,24 @@ describe('getReceiptInfo check', () => {
   it('check return recieptArrayCheck', () => {
 
     const promotion =
-    [
-      {
-        type: 'BUY_TWO_GET_ONE_FREE',
-        barcodes: [
-          'ITEM000000',
-          'ITEM000001',
-          'ITEM000005'
-        ]
-      }
-    ];
-    const recieptArray1 =[{barcode:'ITEM000001',name:'雪碧',unit:'瓶',price:3,num:5},{barcode:'ITEM000003',name:'荔枝',
-      unit:'斤',price:15,num:2.5},{barcode:'ITEM000005',name:'方便面',unit:'袋',price:4.5,num:3}];
-    const recieptArrayCheck =[{"barcode":"ITEM000001","name":"雪碧","unit":"瓶","price":3,"num":5,"sum":15},
-      {"barcode":"ITEM000003","name":"荔枝","unit":"斤","price":15,"num":2.5,"sum":37.5},{"barcode":"ITEM000005","name":"方便面","unit":"袋","price":4.5,"num":3,"sum":13.5}];
-
+      [
+        {
+          type: 'BUY_TWO_GET_ONE_FREE',
+          barcodes: [
+            'ITEM000000',
+            'ITEM000001',
+            'ITEM000005'
+          ]
+        }
+      ];
+    const recieptArray1 = [
+      {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3, num: 5},
+      {barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15, num: 2.5},
+      {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.5, num: 3}];
+    const recieptArrayCheck = [
+      {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3, num: 5, sum: 15},
+      {barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15, num: 2.5, sum: 37.5},
+      {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.5, num: 3, sum: 13.5}];
     spyOn(console, 'log');
 
     let recieptArray = getReceiptInfo(recieptArray1, promotion);
@@ -165,8 +178,10 @@ describe('getReceiptInfo check', () => {
 
     const discountTotalPriceCheck = 66;
 
-    const recieptArray =[{"barcode":"ITEM000001","name":"雪碧","unit":"瓶","price":3,"num":5,"sum":15},
-      {"barcode":"ITEM000003","name":"荔枝","unit":"斤","price":15,"num":2.5,"sum":37.5},{"barcode":"ITEM000005","name":"方便面","unit":"袋","price":4.5,"num":3,"sum":13.5}];
+    const recieptArray = [
+      {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3, num: 5, sum: 15},
+      {barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15, num: 2.5, sum: 37.5},
+      {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.5, num: 3, sum: 13.5}];
 
     spyOn(console, 'log');
 
@@ -180,17 +195,19 @@ describe('generateReciept', () => {
 
   it('should print text', () => {
 
-    const recieptArray =[{"barcode":"ITEM000001","name":"雪碧","unit":"瓶","price":3,"num":5,"sum":12},{"barcode":"ITEM000003","name":"荔枝","unit":"斤","price":15,"num":2.5,"sum":37.5},{"barcode":"ITEM000005","name":"方便面","unit":"袋","price":4.5,"num":3,"sum":9}];
+    const recieptArray = [{barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3, num: 5, sum: 12},
+      {barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15, num: 2.5, sum: 37.5},
+      {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.5, num: 3, sum: 9}];
 
 
     const expectText = `***<没钱赚商店>收据***
-名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
-名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
-----------------------
-总计：58.50(元)
-节省：7.50(元)
-**********************`;
+                        名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+                        名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+                        名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+                        ----------------------
+                        总计：58.50(元)
+                        节省：7.50(元)
+                        **********************`;
     const discountTotalPrice = 58.5;
     const totalPrice = 7.5;
 
